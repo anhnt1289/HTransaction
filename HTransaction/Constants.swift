aa9279f3ce5e0daec98fd7b9861b9a61fbdf5f60
Bip Mac2 <bipmac2@bipmac1.local>
2017-03-08 10:54:48 +0700

    Add File

---
+++ HTransaction/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HTransaction
+//
+//  Created by bipvietnam on 3/8/17.
+//  Copyright © 2017 H-Transaction. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://jokes-d6f0b.firebaseio.com"
+var defaultIndex: Int = 2
---
+++ HTransaction/DataService.swift
@@ -0,0 +1,57 @@
+//
+//  DataService.swift
+//  HTransaction
+//
+//  Created by bipvietnam on 3/8/17.
+//  Copyright © 2017 H-Transaction. All rights reserved.
+//
+
+import Foundation
+import Firebase
+class DataService {
+    static let dataService = DataService()
+    
+    private var _BASE_REF = FIRDatabase.database().referenceFromURL(BASE_URL)
+    private var _USER_REF = FIRDatabase.database().referenceFromURL("\(BASE_URL)/users")
+    private var _JOKE_REF = FIRDatabase.database().referenceFromURL("\(BASE_URL)/jokes")
+    
+    var BASE_REF: FIRDatabaseReference {
+        return _BASE_REF
+    }
+    
+    var USER_REF: FIRDatabaseReference {
+        return _USER_REF
+    }
+    
+    var CURRENT_USER_REF: FIRDatabaseReference {
+        let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+        
+        let currentUser = FIRDatabase.database().referenceFromURL("\(BASE_REF)").child("users").child(userID)
+        
+        return currentUser
+    }
+    
+    var JOKE_REF: FIRDatabaseReference {
+        return _JOKE_REF
+    }
+    
+    func createNewAccount(uid: String, user: Dictionary<String, String>) {
+        
+        // A User is born.
+        
+        USER_REF.child(uid).setValue(user)
+    }
+    
+    func createNewJoke(joke: Dictionary<String, AnyObject>) {
+        
+        // Save the Joke
+        // JOKE_REF is the parent of the new Joke: "jokes".
+        // childByAutoId() saves the joke and gives it its own ID.
+        
+        let firebaseNewJoke = JOKE_REF.childByAutoId()
+        
+        // setValue() saves to Firebase.
+        
+        firebaseNewJoke.setValue(joke)
+    }
+}
